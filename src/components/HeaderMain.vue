57c014044234b70709f2261281e1687ded56cbfe
Roshaan <roshaanmustafa1@gmail.com>
2022-03-17 22:53:47 +0500

    menu making

--- src/components/HeaderMain.vue
+++ src/components/HeaderMain.vue
@@ -18,28 +18,26 @@
 
           <!-- Right aligned nav items -->
           <b-navbar-nav class="ms-auto">
-            <b-nav-form>
-              <b-form-input
-                size="sm"
-                class="mr-sm-2"
-                placeholder="Search"
-              ></b-form-input>
-              <b-button size="sm" class="my-2 my-sm-0" type="submit"
-                >Search</b-button
-              >
-            </b-nav-form>
-
-            <b-nav-item-dropdown text="Lang" right>
-              <b-dropdown-item href="#">EN</b-dropdown-item>
-              <b-dropdown-item href="#">ES</b-dropdown-item>
-              <b-dropdown-item href="#">RU</b-dropdown-item>
-              <b-dropdown-item href="#">FA</b-dropdown-item>
+            <b-nav-item-dropdown
+              text="Lang"
+              class="ms-auto searchDropdown"
+              right
+            >
+              <div class="d-flex">
+                <b-form-input
+                  size="sm"
+                  class="mr-sm-2"
+                  placeholder="Search"
+                ></b-form-input>
+                <b-button size="sm" class="my-2 my-sm-0" type="submit"
+                  >Search</b-button
+                >
+              </div>
             </b-nav-item-dropdown>
-
             <b-nav-item-dropdown right>
               <!-- Using 'button-content' slot -->
               <template #button-content>
-                <em>User</em>
+                <span><b-icon icon="person-fill"></b-icon></span>
               </template>
               <b-dropdown-item href="#">Profile</b-dropdown-item>
               <b-dropdown-item href="#">Sign Out</b-dropdown-item>
@@ -72,5 +70,24 @@ nav.navbar {
 .router-link-active a {
   background: var(--brown-primary) !important;
   color: #fff !important;
+}
+
+.searchDropdown ul.dropdown-menu {
+  width: 500px !important;
+  padding: 0;
+}
+
+.searchDropdown button.btn.my-2.my-sm-0.btn-secondary.btn-sm {
+  background-color: #eb8d2b !important;
+  padding: 5px 25px;
+  color: var(--black-secondary);
+}
+
+.searchDropdown input {
+  border: 1px solid black !important;
+}
+
+input::placeholder {
+  color: black !important;
 }
 </style>
